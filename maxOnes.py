dc5c0a381d7ab7c52337a973ebf40acf74987599
aditya malla thakuri <138849706+red445992@users.noreply.github.com>
2024-11-17 23:57:45 +0545

    max consecutive onesIII

---
+++ maxOnes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left = 0
+        max_length = 0
+        zero_count = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+a = Solution()
+print(a.longestOnes([1,1,1,0,0,0,1,1,1,1,0],2))
